4415905566f7583c60ec9ea86504918e0f8895df
FIRST_NAME LAST_NAME <ruben.tous@gmail.com>
2025-05-08 16:46:45 +0200

    nc

---
+++ data/scenes/disney_101dalmatians_part7/scene_segmentation_settings.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+masks_points = []
+mask0_points = []
+mask0_labels = []
+
+#positive points
+mask0_points.append([600,450])
+mask0_labels.append(1)
+
+mask0_points.append([450,410])
+mask0_labels.append(1)
+
+mask0_points.append([450,600])
+mask0_labels.append(1)
+
+mask0_points.append([450,520])
+mask0_labels.append(1)
+
+mask0_points.append([650,150])
+mask0_labels.append(1)
+
+#negative points
+#mask0_points.append([1250,800]) 
+#mask0_labels.append(0)
+
+masks_points.append([np.array(mask0_points, dtype=np.float32), np.array(mask0_labels, np.int32)])
