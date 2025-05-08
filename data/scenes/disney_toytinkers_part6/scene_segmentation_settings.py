72e1ab1888c69754c5da7789373ae7651c1768b1
FIRST_NAME LAST_NAME <ruben.tous@gmail.com>
2025-05-08 16:48:36 +0200

    nc

---
+++ data/scenes/disney_toytinkers_part6/scene_segmentation_settings.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+masks_points = []
+mask0_points = []
+mask0_labels = []
+
+#positive points
+mask0_points.append([450,500])
+mask0_labels.append(1)
+
+mask0_points.append([1000,500])
+mask0_labels.append(1)
+
+#negative points
+#mask0_points.append([1250,800]) 
+#mask0_labels.append(0)
+
+masks_points.append([np.array(mask0_points, dtype=np.float32), np.array(mask0_labels, np.int32)])
+
+
+
